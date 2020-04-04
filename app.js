a281a83564a4c9477614cddd7999de1081bd4e9d
RTAndrew <anderson_rodax@hotmail.com>
2020-04-04 15:07:02 +0100

    Handle CORS errors

--- app.js
+++ app.js
@@ -24,6 +24,20 @@ app.listen(process.env.PORT || 3000, () => {
     console.log('Server started on PORT ' + process.env.PORT);
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+  }
+  next();
+});
+
+
 // All HTTP 
 app.all('/', (req, res) => {
     console.log(req.body)
