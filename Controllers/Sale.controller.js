242d20c036d471d532537da9b48b140738e7d196
RTAndrew <anderson_rodax@hotmail.com>
2020-04-10 02:53:58 +0100

    Fix: send response data to /sale

--- Controllers/Sale.controller.js
+++ Controllers/Sale.controller.js
@@ -8,7 +8,7 @@ module.exports = {
     getAllSales: async (req, res, next) => {
         try {
             const results = await Sale.find({}, { __v: 0 })
-            res.send("Sales Endpoint");
+            res.send(results);
         } catch (error) {
             console.log(error.message)
         }
